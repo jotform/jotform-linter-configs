b5f0d7e5575b43f1ef0ea12d35734b00d7ec6e35
Berkay Aydin <sbayd06@gmail.com>
2018-05-02 15:36:04 +0300

    Eslint :: update default-export rule

--- eslint-config-jotform/index.js
+++ eslint-config-jotform/index.js
@@ -12,6 +12,7 @@ module.exports = {
   },
   rules: {
     'react/prefer-stateless-function': 0,
+    'import/prefer-default-export': 0,
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-boolean-value': 0,
     'react/no-danger': 0,
